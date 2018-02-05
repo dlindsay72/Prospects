67effba1efe35bcc01f528a9aeeda972691ff6d5
Dan Lindsay <dlindsay72@hotmail.com>
2018-02-05 13:57:32 -0500

    ui for MainVC done, custom CurrencyTextField class created

--- Prospects/Controller/MainVC.swift
+++ Prospects/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  Prospects
 //
 //  Created by Dan Lindsay on 2018-02-05.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Prospects/View/CurrencyTextField.swift
@@ -0,0 +1,28 @@
+//
+//  CurrencyTextField.swift
+//  Prospects
+//
+//  Created by Dan Lindsay on 2018-02-05.
+//  Copyright © 2018 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2528627997)
+        layer.cornerRadius = 20.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+
+}
