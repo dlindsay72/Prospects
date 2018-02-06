6f87666d9ecda829ffb1f822e1b332860ca9ac29
Dan Lindsay <dlindsay72@hotmail.com>
2018-02-06 12:02:36 -0500

    added a label to display currency symbol specific to your locale

--- Prospects/View/CurrencyTextField.swift
+++ Prospects/View/CurrencyTextField.swift
@@ -11,6 +11,22 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.7771828952, green: 0.7771828952, blue: 0.7771828952, alpha: 0.582807149)
+        currencyLbl.textAlignment = .center
+        
+        currencyLbl.textColor = #colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 1)
+        currencyLbl.clipsToBounds = true
+        currencyLbl.layer.cornerRadius = 10.0
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,6 +40,7 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2528627997)
         layer.cornerRadius = 20.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
